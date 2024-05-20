eca443ed35ebb8f207ea997aa8bcb48d8d98aed1
Riky-Droyg <riky.droyg@gmail.com>
2024-05-20 12:27:10 +0300

    main

--- index.js
+++ index.js
@@ -3,31 +3,55 @@ document.addEventListener('DOMContentLoaded', function () {
     let isDragging = false;
     let startX, startY, currentX = 0, currentY = 0;
     
-    document.addEventListener('mousedown', (e) => {
-        if (e.button === 0) {
-            isDragging = true;
-            startX = e.clientX;
-            startY = e.clientY;
-            // Забороняємо виділення тексту під час перетягування
-            document.body.style.userSelect = 'none';
-        }
-    });
+    const startDrag = (x, y) => {
+        isDragging = true;
+        startX = x;
+        startY = y;
+        document.body.style.userSelect = 'none'; // Забороняємо виділення тексту під час перетягування
+    };
 
-    document.addEventListener('mouseup', () => {
+    const stopDrag = () => {
         isDragging = false;
-        // Дозволяємо виділення тексту після відпускання кнопки миші
-        document.body.style.userSelect = '';
-    });
+        document.body.style.userSelect = ''; // Дозволяємо виділення тексту після відпускання кнопки миші або завершення дотику
+    };
 
-    document.addEventListener('mousemove', (e) => {
+    const doDrag = (x, y) => {
         if (isDragging) {
-            const deltaX = e.clientX - startX;
-            const deltaY = e.clientY - startY;
+            const deltaX = x - startX;
+            const deltaY = y - startY;
             currentX -= deltaY * 0.5; // Міняємо знак на "-" для зворотнього руху
             currentY += deltaX * 0.5;
             cube.style.transform = `rotateX(${currentX}deg) rotateY(${currentY}deg)`;
-            startX = e.clientX;
-            startY = e.clientY;
+            startX = x;
+            startY = y;
+        }
+    };
+
+    document.addEventListener('mousedown', (e) => {
+        if (e.button === 0) {
+            startDrag(e.clientX, e.clientY);
+        }
+    });
+
+    document.addEventListener('mouseup', stopDrag);
+
+    document.addEventListener('mousemove', (e) => {
+        doDrag(e.clientX, e.clientY);
+    });
+
+    document.addEventListener('touchstart', (e) => {
+        if (e.touches.length === 1) {
+            const touch = e.touches[0];
+            startDrag(touch.clientX, touch.clientY);
+        }
+    });
+
+    document.addEventListener('touchend', stopDrag);
+
+    document.addEventListener('touchmove', (e) => {
+        if (e.touches.length === 1) {
+            const touch = e.touches[0];
+            doDrag(touch.clientX, touch.clientY);
         }
     });
 });
